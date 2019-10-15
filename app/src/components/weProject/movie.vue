00b0182d91d350f7ff3368e74bd15ffcb7dd497f
xzq <1329800607@qq.com>
2019-10-15 18:51:21 +0800

    Enter

---
+++ app/src/components/weProject/movie.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <mt-tabbar v-model="selected">
+      <mt-tab-item id="tab1">
+        <img src="../../assets/ic_weixin_normal.png">
+        首页
+      </mt-tab-item>
+      <mt-tab-item id="tab2">
+        <img src="../../assets/ic_weixin_normal.png">
+        攻略
+      </mt-tab-item>
+      <mt-tab-item id="tab3">
+        <img src="../../assets/ic_weixin_normal.png">
+        关注
+      </mt-tab-item>
+      <mt-tab-item id="tab4">
+        <img src="../../assets/ic_weixin_normal.png">
+        我
+      </mt-tab-item>
+    </mt-tabbar>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return {
+      selected:"tab1"
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
---
+++ app/src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+//功能：引入第三方组件库mint-ui
+//1.完整引入mint-ui组件库中所有组件
+import MintUi from "mint-ui"
+//2.单引入mint-ui库中样式文件
+import "mint-ui/lib/style.css"
+//3.将mint-ui注册Vue实例
+Vue.use(MintUi)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
