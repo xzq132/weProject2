e5c4319f407e582b42019b551ba89b8d6344f561
xzq <1329800607@qq.com>
2019-10-15 21:16:39 +0800

    enter

---
+++ app/src/components/weProject/common/TabBarIcon.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <img :src="focused?selectedImage:normalImage" alt="" class="isStyle">
+  </div>
+</template>
+<script>
+export default {
+  props:{
+    focused:false,
+    selectedImage:{default:""},
+    normalImage:{default:""}
+  }
+}
+</script>
+<style lang="scss" scoped>
+  .isStyle{
+    width:30px;
+    height:30px;
+  }
+</style>
