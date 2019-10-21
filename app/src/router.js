75957461d173e88ee614fec31e9b7372dbdeb951
lu <1575308685@qq.com>
2019-10-21 21:11:18 +0800

    enter

--- app/src/router.js
+++ app/src/router.js
@@ -6,6 +6,7 @@ import shequheader from './components/weProject/shequheader.vue'
 import Gon from './components/weProject/gon.vue'
 import G from './components/weProject/g.vue'
 import GonChild3 from './components/weProject/gonChild3.vue'
+import tribe from "./components/weProject/tribe.vue"
 
 Vue.use(Router)
 
