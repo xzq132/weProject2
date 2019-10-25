ac492bc90836b942e545f53130f484b34c0445cb
xzq <1329800607@qq.com>
2019-10-25 08:50:19 +0800

    添加到主模块

--- app/src/store.js
+++ app/src/store.js
@@ -5,12 +5,25 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    isLogin:false,   //登录状态
+    phone:""    //登录的手机号
   },
+  // 获取属性的状态
+  getters:{
+    // 获取登录状态
+    isLogin: state => state.isLogin,
+  },
+  // 设置属性状态
   mutations: {
-
+    // 保持登录状态
+    userStatus(state,flag){
+      state.isLogin = flag
+    }
   },
   actions: {
-
+    // 获取登录状态
+    setUser({commit},flag){
+      commit("userStatus",flag)
+    }
   }
 })
